5d2df5b38d5c5fe1d9d7f1152be179707fa399e7
NEEL PATEL <neelpatel3284@gmail.com>
2017-07-12 12:34:37 -0700

    Car Pool with Improved User Interface

--- CarPoolApplication/app/src/main/java/com/carpool/cmpe220/carpoolapplication/MainActivity.java
+++ CarPoolApplication/app/src/main/java/com/carpool/cmpe220/carpoolapplication/MainActivity.java
@@ -17,8 +17,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
 
     /**
